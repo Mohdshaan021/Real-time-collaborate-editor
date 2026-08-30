9271718a32027bb7e3dc6044862db39a12117765
Mohdshaan021 <shaansiddiqui021@gmail.com>
2026-08-30 15:47:06 +0530

    starting the server at bakend only using dist folder

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,8 @@ import {YSocketIO} from "y-socket.io/dist/server"
 const app=express()
 const httpServer=createServer(app)
 
+app.use(express.static("public"))
+
 
 const io=new Server(httpServer,{
     cors:{
